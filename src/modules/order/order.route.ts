a89796c85b9cb279919f7d684e45306b46c43962
amir sabz <amirsabz84@gmail.com>
2024-08-12 01:12:44 +0330

    update order controller and routes

--- src/modules/order/order.route.ts
+++ src/modules/order/order.route.ts
@@ -1,10 +1,19 @@
 import express,{Request,Response,NextFunction,Router} from 'express';
 import * as controller from './order.controller';
+import * as auth from '../../middlewares/user.auth';
 
 const router:Router = express.Router();
 
 router
-    .route('')
-    .post()
+    .route('/show')
+    .get(auth.authUser,controller.createOrder);
+
+router  
+    .route('/submit')
+    .get(auth.authUser,controller.submitOrder);
+
+router 
+    .route('/all')
+    .get(controller.allOrders);
 
 export default router;
