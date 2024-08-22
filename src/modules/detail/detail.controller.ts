2cf8ab32f8ae05e2ec42dfcdd162a317a7d30f23
amir sabz <amirsabz84@gmail.com>
2024-08-23 00:58:08 +0330

    update detail modules

--- src/modules/detail/detail.controller.ts
+++ src/modules/detail/detail.controller.ts
@@ -2,18 +2,19 @@ import express,{Request,Response,NextFunction} from 'express';
 import { Owner,Restaurant,Category,Product,User, PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 import {IGetAuthRequest} from '../../utils/types/req';
+import {calcScore} from '../../utils/helper/calculator_score';
 
 export async function restaurantMenu(req:Request,res:Response){
    const { id } = req.query;
 
-    const restaurant = await prisma.restaurant.findUnique({where:{id:+id}});
+    let restaurant = await prisma.restaurant.findUnique({where:{id:+id}});
     if(!restaurant){
         return res.status(422).json({message:'در بارگذاری رستوران مشکلی پیش امد!'});
     };
 
     const category:Category[] = await prisma.category.findMany({
-        where:{restaurantId:restaurant.id},
-        include:{Product:true}
+        where:{ restaurantId:restaurant.id },
+        include:{ Product:true }
     });
     if(category.length === 0){
         return res
@@ -21,5 +22,11 @@ export async function restaurantMenu(req:Request,res:Response){
             .json({message:'در بارگذاری دسته بندی های رستوران مشکلی پیش امد!'});
     }
 
+    await prisma.restaurant.update({
+        where:{id:+id},
+        data:{watchCount:{increment:1}
+    }});
+    
+    restaurant = calcScore(restaurant);
     return res.status(200).json({restaurant,category});
 };
