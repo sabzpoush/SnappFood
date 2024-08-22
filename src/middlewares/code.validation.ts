0d41074ae28bdb891987e97aaca49bbbe0733688
amir sabz <amirsabz84@gmail.com>
2024-08-23 00:53:27 +0330

    update middlewares
    add email 2fa and sync with frontend

--- src/middlewares/code.validation.ts
+++ src/middlewares/code.validation.ts
@@ -1,3 +1,72 @@
 import express,{Express,Request,Response,NextFunction} from 'express';
-import jwt from 'jsonwebtoken';
+import jwt,{JwtPayload} from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+import {IGetAuthRequest} from '../utils/types/req';
+
+
+export async function validateEmailTest(req:IGetAuthRequest,res:Response,next:NextFunction){
+    const emailToken = req.signedCookies['emailToken'];
+    if(!emailToken){
+        return res.status(422).json({message:'لطفا یکبار دیگر برای احراز ایمیل خود درخواست دهید!'});
+    };
+
+    try{
+        const key:string = process.env.EMAIL_TOKEN;
+        const verifyEmail = jwt.verify(emailToken,key);
+        if(!verifyEmail){
+            return res.status(422).json({message:'توکن نامعتبر است!'});
+        }
+        const decodeEmail = jwt.decode(emailToken) as JwtPayload;
+        const email:string = decodeEmail.email;
+
+        const existEmail = await prisma.user.findFirst({where:{email}});
+        if(!existEmail){
+            return res.status(422).json({message:'این ایمیل قبلا استفاده شده است!'});
+        }
+
+        req.email = email;
+        return next();
+    }catch(e){
+        return res.status(422).json({message:'در اعتبار سنجی کد شما مشکلی رخ داد!'});
+    }
+}
+
+
+export async function validateEmail(req:IGetAuthRequest,res:Response,next:NextFunction){
+    const authHeader:string = req.headers['authorization'] as string;
+    console.log(req.header,req.headers);
+    
+    const emailToken = authHeader.split(' ')[1];
+    if(!emailToken){
+        return res.status(422).json({message:'لطفا یکبار دیگر برای احراز ایمیل خود درخواست دهید!'});
+    };
+    const {userCode} = req.body;
+    try{
+        const key:string = process.env.EMAIL_TOKEN;
+        const verifyEmail = jwt.verify(emailToken,key);
+        if(!verifyEmail){
+            return res.status(422).json({message:'توکن نامعتبر است!'});
+        }
+        const decodeEmail = jwt.decode(emailToken) as JwtPayload;
+        const email:string = decodeEmail.email;
+       
+        // const existEmail = await prisma.user.findFirst({where:{email}});
+        // if(existEmail){
+        //     return res.status(422).json({message:'این ایمیل قبلا استفاده شده است!'});
+        // }
+
+        
+        const code = decodeEmail.code;
+        console.log(code,userCode);
+        if(code != userCode){
+            return res.status(422).json({message:'کد تایید وارده شده اشتباه است!'});
+        }
+
+        req.email = email;
+        return next();
+    }catch(e){
+        return res.status(422).json({message:'در اعتبار سنجی کد شما مشکلی رخ داد!'});
+    }
+}
 
--- src/middlewares/user.auth.ts
+++ src/middlewares/user.auth.ts
@@ -7,8 +7,40 @@ import {IGetAuthRequest} from '../utils/types/req';
 import {JwtPayload} from '../utils/types/jwt';
 
 
-export async function authUser(req:IGetAuthRequest,res:Response,next:NextFunction) {
+export async function authUserTest(req:IGetAuthRequest,res:Response,next:NextFunction) {
     const userToken = req.signedCookies['refreshToken'];
+    if(!userToken){
+        return res.redirect('/user/login');
+    };
+    try{
+        const refreshTokenKey = process.env.REFRESHTOKEN
+        const verifyToken = jwt.verify(userToken,refreshTokenKey);
+        if(!verifyToken){
+            return res.redirect('/owner/login');
+        }
+
+        const decodeToken = jwt.decode(userToken) as JwtPayload;
+        const userId = decodeToken.id;
+
+        const user:User = await prisma.user.findUnique({where:{id:+userId}});
+        if(!user){
+            return res.redirect('/user/login');
+        };
+
+        req.userId = user.id;
+        req.user = user;
+        next();
+    }catch(e){
+        return res.status(419).json({message:e});
+    }
+}
+
+export async function authUser(req:IGetAuthRequest,res:Response,next:NextFunction) {
+    const authHeader:string = req.headers.authorization;
+    if(!authHeader){
+        return res.status(422).json({message:'توکن کاربر ارسال نشده است!',isOk:false});
+    }
+    const userToken = authHeader.split(' ')[1];
     if(!userToken){
         return res.redirect('/user/login');
     };
